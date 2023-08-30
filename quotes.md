c8185a592a7167b98f0e17b14dd53fdf81d53e0f
tahmidachoudhury <tahmidachoudhury@outlook.com>
2023-08-31 00:43:38 +0100

    game logic complete.
    quotes need to match authors.
    
    more features can be added such as:
    - special message after 5 rounds
    - more obious messages for player and ai

---
+++ quotes.md
@@ -0,0 +1,44 @@
+person
+
+“Before we work on artificial intelligence why don’t we do something about natural stupidity?”
+
+—Steve Polyak
+
+“Some people worry that artificial intelligence will make us feel inferior, but then, anybody in his right mind should have an inferiority complex every time he looks at a flower.”
+
+—Alan Kay
+
+
+"“Continue. As the harder you work in training, the less you bleed in war”"
+
+"—idk"
+
+
+
+
+“”
+
+—
+
+
+
+
+
+
+
+ai
+
+“I visualise a time when we will be to robots what dogs are to humans, and I’m rooting for the machines.”
+
+—Claude Shannon
+
+
+
+“Is there anyone that can imitate the work of God? But indeed Mankind have always been pertinacious”
+
+—Someone you love
+
+
+
+"“Today, beaten by a robot. Tomorrow, what will it be?”"
+"—DantheDug"
--- rps.js
+++ rps.js
@@ -9,26 +9,97 @@ function getComputerChoice(){
     return choices[randIndex];
 }
 
+function getPityMessage(player, choice){
+
+    let aiMessages = [
+    "“I visualise a time when we will be to robots what dogs are to humans, and I’m rooting for the machines.”",
+    "“Is there anyone that can imitate the work of God? But indeed Mankind have always been pertinacious”",
+    "“Today, beaten by a robot. Tomorrow, what will it be?”"
+    ];
+
+    let personMessages = [
+    "“Before we work on artificial intelligence why don’t we do something about natural stupidity?”",
+    "“Some people worry that artificial intelligence will make us feel inferior, but then, anybody in his right mind should have an inferiority complex every time he looks at a flower.”",
+    "“Continue. As the harder you work in training, the less you bleed in war”"
+    ];
+
+    let aiAuthors = [
+        "—Claude Shannon",
+        "—Someone you love",
+        "—DantheDug"
+    ]
+
+    let personAuthors = [
+        "—Steve Polyak",
+        "—Alan Kay",
+        "—idk"
+    ]
+
+    let randIndex = Math.floor(Math.random() * aiMessages.length);
+    let i = randIndex
+
+    if (choice == 'quote'){
+        if(player == 'person'){
+            return personMessages[i];
+        }
+
+        if(player == 'ai'){
+            return aiMessages[i];
+        }
+    } if (choice == 'author'){
+        if(player == 'person'){
+            return personAuthors[i];
+        }
+
+        if(player == 'ai'){
+            return aiAuthors[i];
+        }
+    }
+}
+
 
 function playRound(playerSelection){
 
     const computerSelection = getComputerChoice();
     let playerSel = playerSelection.toLowerCase();
+    const results = document.querySelector('.message')
+    const pityMessage = document.querySelector('.quote')
+    const author = document.querySelector('.quote-sauce')
 
     if (playerSel == computerSelection){
-        console.log('tie!');
+        results.innerText = `Your score: ${playerScore} | Computer score: ${compScore} | tie!`; 
     
     } else if (((playerSel == 'paper') && (computerSelection == 'rock'))||
     (((playerSel == 'rock') && (computerSelection == 'scissors')))||
     ((playerSel == 'scissors') && (computerSelection == 'paper'))){
         playerScore += 1;
-        console.log(`you picked ${playerSel}! computer picked ${computerSelection}!
-you win! your score is ${playerScore} out of 3`);       
+        results.innerText = `Your score: ${playerScore} | Computer score: ${compScore}`;
+        pityMessage.innerText = getPityMessage('person', 'quote');
+        author.innerText = getPityMessage('person', 'author');    
 
     } else {
         compScore += 1;
-        console.log(`you picked ${playerSel}! computer picked ${computerSelection}! 
-you lose! computer's score is ${compScore} out of 3`);}
+        results.innerText = `Your score: ${playerScore} | Computer score: ${compScore}`; }
+        pityMessage.innerText = getPityMessage('ai', 'quote');
+        author.innerText = getPityMessage('ai', 'author');
+
+    if (playerScore == 5){
+        results.innerText = `You win.`; 
+        playerScore = 0;
+        compScore = 0;
+        pityMessage.innerText = getPityMessage('person', 'quote');
+        author.innerText = getPityMessage('person', 'author');
+    } if (compScore == 5){
+        results.innerText = `You lose.`; 
+        playerScore = 0;
+        compScore = 0;
+        pityMessage.innerText = getPityMessage('ai', 'quote');
+        author.innerText = getPityMessage('ai', 'author');
+    } if ((playerScore == 0)||(compScore == 0)){
+        pityMessage.innerText.remove;
+        author.innerText.remove;
+    }
+
 
 
     const rockAIButton = document.querySelector('.AI.rock');
@@ -68,39 +139,3 @@ function removeTransition(e) {
 
 options = document.querySelectorAll('.player, .AI');
 options.forEach(option => option.addEventListener('transitionend', removeTransition));
-
-
-
-
-//        function game(){
-//            roundStarter(true)
-//            roundStarter(true)
-//            roundStarter(true)
-//            roundStarter(true)
-//            roundStarter()
-//        }
-
-function roundStarter(finish){
-    let playerSelection = String(prompt("pick either 'rock', 'paper' or 'scissors'"));
-        if (playerSelection == "null"){
-            playerSelection = "nothing";}
-
-    playRound(playerSelection, computerSelection);
-
-    if (finish == null || finish == false){
-        if (compScore > playerScore) {
-        console.log(`That's the end! i beat you with the final score:
-you: ${playerScore}
-me: ${compScore}`)
-        } else if (playerScore > compScore){
-        console.log(`That's the end! you beat me with the final score:
-you: ${playerScore}
-me: ${compScore}`)
-        } else{
-        console.log(`tie! 
-me: ${compScore}
-you: ${playerScore}
-press control+r RIGHT NOW!`)
-        }
-    }
-}
